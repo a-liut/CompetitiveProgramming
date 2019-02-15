dbcc57cd86e82ab008e20d74e39e8f84ea9b9e59
a.liut <andrea.liut@gmail.com>
2019-02-15 10:58:14 +0100

    Bugfix Powerful Array

--- Powerful Array/main.cpp
+++ Powerful Array/main.cpp
@@ -12,29 +12,15 @@ struct query_t
 
 int64_t power(int64_t val, int64_t occurrencies)
 {
-	return val * (occurrencies * occurrencies);
-}
-
-void add(std::vector<int64_t> &items, int64_t *occurrencies, int64_t pos, int64_t &answer)
-{
-	int64_t s = power(items[pos], occurrencies[items[pos]] + 1) - power(items[pos], occurrencies[items[pos]]);
-	answer += s;
-	occurrencies[items[pos]]++;
-}
-
-void remove(std::vector<int64_t> &items, int64_t *occurrencies, int64_t pos, int64_t &answer)
-{
-	int64_t s = power(items[pos], occurrencies[items[pos]]) - power(items[pos], occurrencies[items[pos]] - 1);
-	occurrencies[items[pos]]--;
-	answer -= s;
+	return val * (pow(occurrencies, 2));
 }
 
 void powerful_array(std::vector<int64_t> items, std::vector<query_t> queries)
 {
-	int64_t n = MAX_EL;
+	int64_t n = MAX_EL, s;
 	int64_t current_l = 0, current_r = 0, answer = 0, block = (int64_t)sqrt(items.size());
-	int64_t occurrencies[n];
-	int64_t answers[queries.size()];
+	
+	int64_t occurrencies[n], answers[queries.size()];
 
 	for (int64_t i = 0; i < n; ++i)
 	{
@@ -59,25 +45,35 @@ void powerful_array(std::vector<int64_t> items, std::vector<query_t> queries)
 	{
 		while (current_l < q.l)
 		{
-			remove(items, occurrencies, current_l, answer);
+			s = power(items[current_l], occurrencies[items[current_l]]) - power(items[current_l], occurrencies[items[current_l]] - 1);
+			occurrencies[items[current_l]]--;
+			answer -= s;
+
 			current_l++;
 		}
 
 		while (current_l > q.l)
 		{
 			current_l--;
-			add(items, occurrencies, current_l, answer);
+
+			answer += power(items[current_l], occurrencies[items[current_l]] + 1) - power(items[current_l], occurrencies[items[current_l]]);
+			occurrencies[items[current_l]]++;
 		}
 
 		while (current_r < q.r)
 		{
 			current_r++;
-			add(items, occurrencies, current_r, answer);
+
+			answer += power(items[current_r], occurrencies[items[current_r]] + 1) - power(items[current_r], occurrencies[items[current_r]]);
+			occurrencies[items[current_r]]++;
 		}
 
 		while (current_r > q.r)
 		{
-			remove(items, occurrencies, current_r, answer);
+			s = power(items[current_r], occurrencies[items[current_r]]) - power(items[current_r], occurrencies[items[current_r]] - 1);
+			occurrencies[items[current_r]]--;
+			answer -= s;
+
 			current_r--;
 		}
 		
@@ -93,7 +89,7 @@ void powerful_array(std::vector<int64_t> items, std::vector<query_t> queries)
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(0);
+	std::cin.tie(NULL);
 
 	int64_t n, t, x, i;
 	std::vector<int64_t> items;
@@ -101,16 +97,16 @@ int main()
 
 	std::cin >> n >> t;
 
-	items.reserve(n);
-	items.push_back(0);
-	for (i = 0; i < n; i++)
+	items.resize(n + 1);
+	items[0] = 0;
+	for (i = 1; i <= n; i++)
 	{
 		std::cin >> x;
 
-		items.push_back(x);
+		items[i] = x;
 	}
 
-	queries.reserve(t);
+	queries.resize(t);
 	for (i = 0; i < t; i++)
 	{
 		query_t q;
@@ -123,7 +119,7 @@ int main()
 
 		q.idx = i;
 
-		queries.push_back(q);
+		queries[i] = q;
 	}
 
 	powerful_array(items, queries);
