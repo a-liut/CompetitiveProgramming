bd79ceffbd5cd1f50919292421e0450bb52b0777
a.liut <andrea.liut@gmail.com>
2019-02-21 11:09:01 +0100

    Fixed Pashmak and Parmidas problem

--- Pashmak and Parmida/main.cpp
+++ Pashmak and Parmida/main.cpp
@@ -3,15 +3,17 @@
 #include <algorithm>
 #include <math.h>
 
+using namespace std;
+
 template <class T>
 class bit
 {
   private:
-	std::vector<T> elems;
+	vector<T> elems;
 	int64_t size;
 
   public:
-	typedef typename std::vector<T>::iterator iterator;
+	typedef typename vector<T>::iterator iterator;
 
 	bit(int64_t size): size(size)
 	{
@@ -57,27 +59,24 @@ class bit
 
 		return sb - sa;
 	}
-
-	inline iterator begin() noexcept { return elems.begin(); }
-	inline iterator end() noexcept { return elems.end(); }
 };
 
-int64_t pashmak(std::vector<int64_t> &elems)
+int64_t pashmak(vector<int64_t> &elems)
 {
 	int64_t i;
 
 	// Map values into range [0-1]
-	std::vector<int64_t> allElems(elems);
+	vector<int64_t> allElems(elems);
 
-	std::sort(allElems.begin(), allElems.end());
+	sort(allElems.begin(), allElems.end());
 
 	for(int64_t &el : elems)
 	{
-		el = std::lower_bound(allElems.begin(), allElems.end(), el) - allElems.begin();
+		el = lower_bound(allElems.begin(), allElems.end(), el) - allElems.begin();
 	}
 
 	// Compute the SuffixCounter array: SC[i] = f(j, n, elems[j])
-	std::vector<int64_t> suffixCounter(elems.size(), 0), counter(elems.size(), 0);
+	vector<int64_t> suffixCounter(elems.size(), 0), counter(elems.size(), 0);
 	bit<int64_t> tree(suffixCounter.size());
 	i = elems.size() - 1;
 	for(i = elems.size() - 1; i >= 0; --i)
@@ -88,7 +87,7 @@ int64_t pashmak(std::vector<int64_t> &elems)
 	}
 
 	// Reuse the same counter vector object
-	std::fill(counter.begin(), counter.end(), 0);
+	fill(counter.begin(), counter.end(), 0);
 	int64_t res = 0;
 
 	// Compute f(1, i, A[i])
@@ -104,20 +103,20 @@ int64_t pashmak(std::vector<int64_t> &elems)
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	int64_t n, x;
-	std::vector<int64_t> elems;
+	vector<int64_t> elems;
 
-	std::cin >> n;
+	cin >> n;
 	elems.resize(n, 0);
 
 	for (int64_t i = 0; i < n; i++)
 	{
-		std::cin >> x;
+		cin >> x;
 		elems[i] = x;
 	}
 
-	std::cout << pashmak(elems) << std::endl;
+	cout << pashmak(elems) << endl;
 }
